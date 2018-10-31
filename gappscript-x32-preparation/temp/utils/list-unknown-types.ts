77fa7f5ec944a03e787ca0fbb9b961d355c37eac
Carsten Koch <carstenkoch@Carstens-MBP.fritz.box>
2018-10-31 21:35:32 +0100

    deleted temp files

--- gappscript-x32-preparation/temp/utils/list-unknown-types.ts
+++
@@ -1,6 +0,0 @@
-const listUnknownTypes = (types, channels) => {
-    types.splice(0,1); // slice header
-    if (channels.length === 1) channels = channels[0];
-    const arr = types.filter(t => channels.filter(c => t == c).length == 0);
-    return arr;
-}
